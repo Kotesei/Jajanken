5b46eb089036e9ef07911a268d7d2efd3e4cf4a6
Kotesei <192398548+Kotesei@users.noreply.github.com>
2024-12-22 23:16:20 -0500

    Made more changes
    Added the h2 element above the game, it now mentions the current round and set rounds
    
    Next goals:
    Change the scoreboard so that it can tally the rounds
    Make an input for the user that'll reveal itself when pressing Play now button
    Change "Pick one" to the player option they picked.
    
    Low Priority:
    Adding animation for NPC when it plays
    Add a delay so it seems the NPC is thinking.
    Add a win/loss/tie response from the NPC when playing against it.

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 
 // HTML elements
 const buttons = document.querySelectorAll("button")
+const playBtn = document.querySelector(".play--btn")
 const npcBtn = document.querySelector(".npc--btn")
+const body = document.querySelector(".body")
 const wins = document.querySelector(".wins")
 const losses = document.querySelector(".losses")
 const ties = document.querySelector(".ties")
+const npcScore = document.querySelector(".npcPoints");
+const playerScore = document.querySelector(".playerPoints");
 
 // Jajanken values
 const jajankenValues = {
@@ -18,22 +22,44 @@ const jajankenValues = {
 let playerWins = 0;
 let playerLosses = 0;
 let playerTies = 0;
+let curRounds = 0;
+
 
 // Function to start Rock, Paper, Scissors
-function jajanken(npc, player, rounds) {
+function jajanken(npc, player, maxRounds) {
+    // Stop game if current round reaches maxRounds
+    if (curRounds >= maxRounds) {
+        // See if player or NPC has more and say who wins.
+        return
+    };
+    curRounds++
+
+    // Shows user the rounds.
+    body.firstChild.innerHTML = `Round: <span class="setRounds">${curRounds} / ${maxRounds}</span>`;
+   
+// Allow NPC choice to fade in/out and only play once it's called for.
+
+// NPC button is revealed based off the choice they made.
 npcBtn.style.backgroundColor = "white"
 npcBtn.style.backgroundImage = `url(./images/${jajankenValues[npc]}.gif)`
+
 // Handles losing
 if (npc > player && !(player === 0 && npc === 2) || player === 2 && npc === 0) {
     playerLosses++
     console.log(`You Lose! ${jajankenValues[npc]} beats ${jajankenValues[player]}`);
     losses.innerHTML = ` ${playerLosses}`
+    npcScore.innerHTML++
+
 }
+
+
 // Handles tie
 if (npc === player) {
     playerTies++;
     console.log(`It's a tie! You both picked ${jajankenValues[npc]}`);
     ties.innerHTML = ` ${playerTies}`
+    
+    
 }
 
 // Handles win
@@ -41,6 +67,7 @@ if (npc < player && !(player === 2 && npc === 0) || player === 0 && npc === 2) {
     playerWins++
     console.log(`You win! ${jajankenValues[player]} beats ${jajankenValues[npc]}`);
     wins.innerHTML = ` ${playerWins}`
+    playerScore.innerHTML++
 }
 
 console.log(`That makes ${playerWins} wins, ${playerLosses} losses, and ${playerTies} ties`);
@@ -62,31 +89,50 @@ function setRounds(num) {
     return Number(num)
 }
 
-// Gives each button an ID, added - 1 to ignore the last button
-for (let i = 0; i < buttons.length - 1; i++) {
-    buttons[i].id = i
-    buttons[i].style.backgroundImage = `url(./images/idle.png)`;
 
-    buttons[i].addEventListener('mouseenter', function(e) {
-        
-        e.target.style.backgroundImage = `url(./images/${jajankenValues[e.target.id]}.gif)`;
-    })
 
-    buttons[i].addEventListener('mouseleave', function(e) {
-        e.target.style.backgroundImage = `url(./images/idle.png)`;
+//////////////////
 
-        
-    })
+// Add a h2 element above the game that mentions which round it is currently on.
 
-    
+
+// Create a Play button that takes user input for rounds.
+playBtn.addEventListener("click", function() {
+    const roundsContainer = `<h2>Start Game!!!<span class="setRounds"></span></h2>`
+    body.insertAdjacentHTML("afterbegin", roundsContainer)
+   
+
+    for (let i = 0; i < buttons.length; i++) {
+        // If button has either the play--btn or npc--btn don't continue
+           if (buttons[i].classList.contains("npc--btn") || buttons[i].classList.contains("play--btn")) return
+
+        // Give each button an ID
+           buttons[i].id = i
+           
+           // Give each choice a static fist image
+           buttons[i].style.backgroundImage = `url(./images/idle.png)`;
+
+           // On mouse enter (hover) it'll play the gif
+            buttons[i].addEventListener('mouseenter', function(e) {
+                
+                e.target.style.backgroundImage = `url(./images/${jajankenValues[e.target.id]}.gif)`;})
+
+            // On mouse exit it'll switch back to static image
+            buttons[i].addEventListener('mouseleave', function(e) {
+
+                e.target.style.backgroundImage = `url(./images/idle.png)`;})
+
+            // Runs the function to get the NPC choice and Player choice
+            buttons[i].addEventListener("click", function() {
+
+                jajanken(getComputerChoice(), getPlayerChoice(buttons[i].id), setRounds(5))
+        });
+
 }
 
-// For each button we added an event listener that calls both NPC, and Player choices.
-buttons.forEach(button => {
+// Only listen once.
+}, {once: true})
 
-    // Don't add event listener to npc button
-    if (button.classList.contains("npc--btn")) return
-    button.addEventListener("click", function() {
-        jajanken(getComputerChoice(), getPlayerChoice(button.id))
-    });
-})
+
+// Change the scoreboard so that it can tally the rounds and add the score under the players.
+//////////////////
