c72a44f50596700668a14538861c955969951b08
Kotesei <192398548+Kotesei@users.noreply.github.com>
2024-12-23 14:18:39 -0500

    Bug fixing, setting timeouts

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const buttons = document.querySelectorAll("button")
 const playBtn = document.querySelector(".play--btn")
 const npcBtn = document.querySelector(".npc--btn")
+const playerAction = document.querySelector(".player--action")
+const npcAction = document.querySelector(".npc--action")
+const revealBtns = document.querySelectorAll('.container--opponent button')
 const body = document.querySelector(".body")
 const wins = document.querySelector(".wins")
 const losses = document.querySelector(".losses")
@@ -25,12 +28,42 @@ let playerLosses = 0;
 let playerTies = 0;
 let curRounds;
 
-function animateBtns(gameState) {
-    // Get the buttons
-    const revealBtns = document.querySelectorAll('.container--opponent button')
+// NPC responses
+const brainrotResponses = {
+    win: [
+        "HA! Your pixels are mine! Bow before my superiority, you blinking potato!",
+        "I win, I grin, and now I spin!",
+        "Victory tastes like... burnt toast and regret. Delicious!",
+        "DID YOU EVEN TRY?! Or was that your cat playing for you?!",
+        "I am the banana in your cereal! The ultimate disruptor!"
+    ],
+    loss: [
+        "WHAT?! My hands are defective! My brain forgot the controls!",
+        "This is fake! Rigged! The simulation hates me!",
+        "You cheated. Or I let you win. Or both. Yeah, both!",
+        "Nooooo! My dignity! My nonexistent dignity!",
+        "It’s fine, I didn’t want to win anyway... (quiet sobbing)... totally fine."
+    ],
+    tie: [
+        "A draw?! What are we, evenly matched spaghetti noodles?!",
+        "Guess we both suck! Or we’re both amazing. One of those!",
+        "Tie? More like... uh... well, I don’t have a pun. Whatever.",
+        "So... does this mean we’re best friends now? Or worst enemies?",
+        "A tie? I wanted glory, not... mediocrity! Ugh!"
+    ]
+};
+
+let timeout;
+function animateBtns(gameState, npc) {
+    clearTimeout(timeout)
  
     // Calls animateBtns when game starts and reveals all buttons
     if (gameState === 'start') {
+
+        playerAction.innerHTML = "";
+        npcAction.innerHTML = "";
+        npcBtn.style.backgroundImage = `url(./images/idle.png)`
+        npcBtn.style.transition = "bottom 2s ease, opacity 5s"
     revealBtns.forEach(btn => {
         btn.style.opacity = "1"
         btn.style.bottom = "0px"
@@ -38,15 +71,38 @@ function animateBtns(gameState) {
 
     return
 }
+
+
+// Ongoing game 
+if (gameState === 'ongoing') {
+    console.log("still going");
+    npcBtn.style.transition = "all 0s"
+   
+    npcBtn.style.backgroundImage = `url("./images/${jajankenValues[npc]}.gif")`
+
+    timeout = setTimeout(() => {
+        npcBtn.style.transition = "all 1s";
+        npcBtn.style.backgroundImage = `url("./images/rock.gif")`;
+
+        npcAction.innerHTML = "";
+        console.log('Timed Out');
+    }, 2000);
+
+
+    
+}
     // Calls animateBtns when curRounds >= state
   if (gameState === 'end') {
-    endGame();
-    revealBtns.forEach(btn => {
-        btn.style.opacity = "0"
-        btn.style.bottom = "-100px"
-       })
-
-       return
+    setTimeout(() => {
+        npcBtn.style.transition = "opacity 5s"
+        revealBtns.forEach(btn => {
+            btn.style.opacity = "0"
+            btn.style.bottom = "-100px"
+            
+           })
+           return
+    }, 2000)
+   
   }
     
   
@@ -56,21 +112,24 @@ function animateBtns(gameState) {
 function jajanken(npc, player, maxRounds) {
     curRounds++
     // Shows user the rounds.
-    
+    animateBtns("ongoing", npc)
+
+
     // Stop game if current round reaches maxRounds
     if (curRounds >= maxRounds) {
-        animateBtns('end');
+        endGame();
+        
         // See if player or NPC has more and say who wins.
         updateScore(npc, player, maxRounds);
         
     } else {
+        
         body.firstChild.innerHTML = `Round: <span class="setRounds">${curRounds + 1} / ${maxRounds}</span>`;
    
         // Allow NPC choice to fade in/out and only play once it's called for.
         
         // NPC button is revealed based off the choice they made.
-        npcBtn.style.backgroundColor = "white"
-        npcBtn.style.backgroundImage = `url(./images/${jajankenValues[npc]}.gif)`
+   
         
         // Check who wins / loses
         updateScore(npc, player, maxRounds); 
@@ -94,6 +153,8 @@ function setRounds(num) {
 
 // Function to update score
 function updateScore(npc, player, maxRounds) {
+    playerAction.innerHTML = `You picked ${jajankenValues[player]}!`
+    npcAction.innerHTML = jajankenValues[npc]
 
     function lose() {
         console.log(`You Lose! ${jajankenValues[npc]} beats ${jajankenValues[player]}`);
@@ -109,19 +170,23 @@ function updateScore(npc, player, maxRounds) {
     function compareScore() {
         const playerPoints = playerScore.innerHTML
         const npcPoints = npcScore.innerHTML
+        const randomIndex = Math.floor(Math.random() * 4)
 
         if (playerPoints > npcPoints) {console.log("You won the game!")
             playerWins++
             wins.innerHTML = ` ${playerWins}`
-
+            npcAction.innerHTML = brainrotResponses.loss[randomIndex];
+            
         };
         if (playerPoints === npcPoints) {console.log("Nobody won!")
             playerTies++
-            ties.innerHTML = ` ${playerTies}`
-        };
-        if (playerPoints < npcPoints) {console.log("You lost the game!")
-            playerLosses++
-            losses.innerHTML = ` ${playerLosses}`
+        ties.innerHTML = ` ${playerTies}`
+        npcAction.innerHTML = brainrotResponses.tie[randomIndex]
+    };
+    if (playerPoints < npcPoints) {console.log("You lost the game!")
+        playerLosses++
+    losses.innerHTML = ` ${playerLosses}`
+    npcAction.innerHTML = brainrotResponses.win[randomIndex]
         };
     }
 
@@ -167,6 +232,7 @@ function playerChoice(userInput, rounds) {
 
 // Create a Play button that takes user input for rounds.
 function startGame() {
+    animateBtns('start')
     let gameRounds = setRounds(5)
     curRounds = 0;
     npcScore.innerHTML = 0
@@ -179,7 +245,7 @@ function startGame() {
         else roundsContainer.outerHTML = insertRounds
         roundsContainer = document.querySelector(".roundh2")
 
-    animateBtns('start')
+        
     
     for (let i = 0; i < buttons.length; i++) {
         // If button has either the play--btn or npc--btn don't continue
@@ -217,6 +283,7 @@ function startGame() {
 
 // ends the current game
 function endGame() {
+    animateBtns('end');
     for (let i = 0; i < buttons.length; i++) {
         // If button has either the play--btn or npc--btn don't continue
         if (buttons[i].classList.contains("npc--btn") || buttons[i].classList.contains("play--btn")) return 
@@ -233,6 +300,8 @@ function endGame() {
     
     }
     
+   
+
 
 }
 
